b8d127e38a0b83d8388400e2abe17caa7a4d4f69
Bolimbo <carlo.cappellini@yahoo.com>
2018-02-05 23:19:44 -0600

    practicing js

---
+++ js/external.js
@@ -0,0 +1,17 @@
+"use strict";
+
+console.log("hello from external JS");
+
+alert("Welcome to my site");
+
+var question = prompt("what is you name?");
+
+if (question == null) {
+alert ("null is not name")
+alert("have a great day")
+    }
+    else if (question){
+        alert(question + " is a beautiful name");
+    }
+
+
