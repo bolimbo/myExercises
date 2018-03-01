0853f8818fc89eb9d46148cd2ea21157d9e29a37
Bolimbo <carlo.cappellini@yahoo.com>
2018-02-28 22:32:35 -0600

    worked on loops, need to do loops over

--- js/while.js
+++ js/while.js
@@ -7,21 +7,22 @@
 //     console.log(i);
 //
 // }
-
-var allCones = Math.floor(Math.random() * 50) + 50
-var order;
-
-console.log("I start my day with " + allCones + " cones");
-do {
-    order = Math.floor(Math.random() * 5) + 1;
-    if (order <= allCones){
-        allCones = allCones - order;
-        console.log("cones sold " + order)
-
-    } else {
-        console.log(" I cannot sell you " + order + " cones. I only have " + allCones + " left")
-    }
-
-} while(allCones > 0){
-console.log("I sold all ice cream cones")
-}
+// =========
+// var allCones = Math.floor(Math.random() * 50) + 50
+// var order;
+//
+// console.log("I start my day with " + allCones + " cones");
+// do {
+//     order = Math.floor(Math.random() * 5) + 1;
+//     if (order <= allCones){
+//         allCones = allCones - order;
+//         console.log("cones sold " + order)
+//
+//     } else {
+//         console.log(" I cannot sell you " + order + " cones. I only have " + allCones + " left")
+//     }
+//
+// } while(allCones > 0){
+// console.log("I sold all ice cream cones")
+// }
+// =========
