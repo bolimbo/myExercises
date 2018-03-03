2194f3fcc323a75a0336090ddd44d5f469b87e26
Bolimbo <carlo.cappellini@yahoo.com>
2018-03-03 00:14:10 -0600

    mastering loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -33,11 +33,41 @@
 //     console.log(i.repeat(i))
 // }
 
-for (var i = 100; i >= 5; i -= 5){
+// for (var i = 100; i >= 5; i -= 5){
+//     console.log(i)
+//
+// }
+
+//
+// function  mT(x) {
+//     for(var i = 0; i <= 10; i++ ){
+//         console.log(x + " x " + i  + " = " + (x *i))
+//     }
+//
+// }
+// mT(3);
+
+//
+// var number;
+//
+// for(var i = 0; i <= 10; i++){
+//    number = Math.floor(Math.random() * 20) + 180;
+//
+//    if (number % 2 === 0){
+//        console.log(number + " even")
+//
+//    }else{
+//        console.log(number+"odd")
+//    }
+//
+// }
+
+// for(var i = 0; i < 10; i++){
+//    i =  i.toString()
+//     console.log(i.repeat(i))
+// }
+//
+
+for ( var i = 100;i > 0; i-=5){
     console.log(i)
-
 }
-
-
-
-
