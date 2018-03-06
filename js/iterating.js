fdd0828f12ab632d56a60c3c1756d968515c2edd
Bolimbo <carlo.cappellini@yahoo.com>
2018-03-06 07:38:57 -0600

    practicing Arrays

--- js/iterating.js
+++ js/iterating.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
     "use strict";
 
     /**
@@ -6,43 +6,39 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
-    var names = ["Luis", "John", "Brad", "Nilo"];
-
+    var names = ["Carlo", "Jessica", "Ted", "Bob"]
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
-     // */ console.log(names.length);
-
+     */
+    console.log("total number of names is " + names.length)
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
-     *
      */
-    // console.log(names[0]);
-    // console.log(names[1]);
-    // console.log(names[2]);
-    // console.log(names[3]);
+    console.log(names[0])
+    console.log(names[1])
+    console.log(names[2])
+    console.log(names[3])
 
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-    // for (var i = 0; i < names.length; i++){
-    //     console.log(names[i]);
-    // }
-
-
+    for (var i = 0; i < names.length; i++) {
+        console.log(names[i])
+    }
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
-    // names.forEach(function (name){
-    //     console.log(name)
-    // });
+    names.forEach(function (name) {
+        console.log(name)
 
+    });
 
     /**
      * TODO:
@@ -57,38 +53,38 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
-    function first (array) {
-        if (Array.isArray(array)){
+
+    function first(array) {
+        if (Array.isArray(array)) {
             return array[0]
         }
+
     }
 
-    function second (array) {
-        if(Array.isArray(array)){
+    function second(array) {
+        if (Array.isArray(array)) {
             return array[1]
         }
 
     }
 
-    function last (array) {
-        if (Array.isArray(array)){
-            return array [-1]
+    function last(array) {
+        if (Array.isArray(array)) {
+            return array[array.length - 1]
         }
 
     }
 
-
-    function nextToLast(array) {
-        return array[array.length -2];
+    function nextTolast(array) {
+        return array[array.length - 2];
     }
 
-    console.log(first([1, 2, 3, 4, 5]));
-    console.log(second([6, 7, 8, 9, 10]));
+   console.log( first([1, 2, 3, 4, 5]));
+    console.log( second([1, 2, 3, 4, 5]));
+    console.log(last([1, 2, 3, 4, 5]));
+    console.log(nextTolast(names))
 
-    console.log(last(names));
-    console.log(nextToLast(names));
-
-    console.log(nextToLast([1, 2, 3, 4, 5, 6]));
 
 
 })();
+
